a4d7916294f0e394ead0388e9c9c6d0acf8f6150
Szabo Rihcard <ignocius@gmail.com>
2017-01-11 10:33:27 +0100

    validete written but not working yet

--- test.js
+++ test.js
@@ -1,6 +1,25 @@
 'use strict';
 
 var test = require('tape');
+var validator = require('./validate')
+
+test('first test', function(t) {
+  let actual = (true);
+  let expected = true;
+  t.equal(actual, expected);
+  t.end();
+});
+
+
+test('validate a good email', function (t) {
+  t.equal(decrypt()'alma'), 'alma');
+  t.end();
+})
+
+test('decrypt alma with 1', function (t) {
+  t.equal(decrypt(1, 'zold'), 'ynkc');
+  t.end();
+})
 
 test("validator accepts valid email, a scale of 15 and positive feedback");
 test("validator rejects unfilled email, a scale of 15 and positive feedback");
